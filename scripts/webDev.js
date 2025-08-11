be192201ac8b64afcce9fe92e601e9a576834575
NightLocke15 <christinefrances.holt@gmail.com>
2025-08-12 00:53:33 +0200

    Game Dev and About | Pages and their content added

--- scripts/webDev.js
+++ scripts/webDev.js
@@ -5,22 +5,14 @@ const webDevItems = [
         img: "../images/main/picture.jpg",
         href: "",
         status: "In Progress"
-    },
+    },   
     {
-        name: "First Portfolio Website",
-        github: "https://github.com/Wits-Digital-Arts-Interactive-Media/WSOA3028A_2615283",
-        link: "https://wits-digital-arts-interactive-media.github.io/WSOA3028A_2615283/",
-        img: "../images/webDev/ChristineCharacter.jpg",
+        name: "NUDGE Mobile App",
+        github: "https://github.com/NightLocke15/Nudge-Mobile-App",
+        link: "",
+        img: "../images/webDev/Nudge.png",
         href: "",
-        status: "Completed"
-    },
-    {
-        name: "Step 1",
-        github: "https://github.com/NightLocke15/WSOA3029A_2615283_ChristineHolt_CommercialWebsite",
-        link: "https://nightlocke15.github.io/WSOA3029A_2615283_ChristineHolt_CommercialWebsite/",
-        img: "../images/webDev/StepOne.png",
-        href: "",
-        status: "Being Revamped"
+        status: "In Progress" 
     },
     {
         name: "The Character Collection",
@@ -31,14 +23,21 @@ const webDevItems = [
         status: "In Progress" 
     },
     {
-        name: "NUDGE Mobile App",
-        github: "https://github.com/NightLocke15/Nudge-Mobile-App",
-        link: "",
-        img: "../images/webDev/Nudge.png",
+        name: "Step 1",
+        github: "https://github.com/NightLocke15/WSOA3029A_2615283_ChristineHolt_CommercialWebsite",
+        link: "https://nightlocke15.github.io/WSOA3029A_2615283_ChristineHolt_CommercialWebsite/",
+        img: "../images/webDev/StepOne.png",
         href: "",
-        status: "In Progress" 
+        status: "Being Revamped"
     },
-    
+    {
+        name: "First Portfolio Website",
+        github: "https://github.com/Wits-Digital-Arts-Interactive-Media/WSOA3028A_2615283",
+        link: "https://wits-digital-arts-interactive-media.github.io/WSOA3028A_2615283/",
+        img: "../images/webDev/ChristineCharacter.jpg",
+        href: "",
+        status: "Completed"
+    },    
 ]
 
 const itemsSection = document.querySelector('.webDevItemsContainer');
